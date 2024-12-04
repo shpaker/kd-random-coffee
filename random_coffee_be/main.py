9c6c2faca56e0587c4113f32f65d10044a7df22e
luch1x <146944858+luch1x@users.noreply.github.com>
2024-12-04 10:34:06 +0200

    Update main.py

--- random_coffee_be/main.py
+++ random_coffee_be/main.py
@@ -1,14 +1,13 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator, Any
-from unittest.mock import Base
 
 from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-from random_coffee_be.transports.views.clubs.views import clubs_router
-from random_coffee_be.transports.views.common.views import common_router
-from random_coffee_be.transports.views.users.views import users_router
+from .transports.common.views import common_router
+from .views.users_views import users_router
 
 # Настройки базы данных
 DATABASE_URL = "postgresql+asyncpg://luch1x:Fq12Ww@localhost:5432/KODE-practice"
@@ -17,31 +16,31 @@ DATABASE_URL = "postgresql+asyncpg://luch1x:Fq12Ww@localhost:5432/KODE-practice"
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 # Создание асинхронной сессии
-async_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
+# Создание базовой модели для SQLAlchemy
+Base = declarative_base()
+
+# Асинхронный контекстный менеджер для управления жизненным циклом приложения
 @asynccontextmanager
-async def _lifespan(
-    app: FastAPI,
-) -> AsyncGenerator[dict[str, Any], None]:
+async def _lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     print(f"OUR FRIENDLY {app} STARTUP")
-    # Инициализация базы данных
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield {
-        "db_engine": engine,
-    }
-    print(f"OUR FRIENDLY {app} SHUTDOWN")
-    # Закрытие движка
-    await engine.dispose()
+    
+    # Инициализируем базу данных
+    async with engine.begin() as transaction:
+        await transaction.run_sync(Base.metadata.create_all)
+        
+    try:
+        yield
+    finally:
+        print(f"OUR FRIENDLY {app} SHUTDOWN")
+        await engine.dispose()
 
-def make_app() -> FastAPI:
-    app = FastAPI(
-        lifespan=_lifespan,
-    )
-    app.include_router(common_router)
-    app.include_router(users_router)
-    app.include_router(clubs_router)
-    return app
+# Создание FastAPI приложения
+app = FastAPI(lifespan=_lifespan)
 
+# Добавляем маршруты из common_router
+app.include_router(common_router)
+
+# Добавляем маршруты из users_router
+app.include_router(users_router)
