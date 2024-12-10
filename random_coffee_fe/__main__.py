c42341d52a78d414c59833c9547ff256f74ea6b9
MeMnO <163781459+MeMnO228@users.noreply.github.com>
2024-12-10 12:47:36 +0200

    Delete random_coffee_fe directory

--- random_coffee_fe/__main__.py
+++
@@ -1,5 +0,0 @@
-from aiogram import executor
-from random_coffee_fe.intagrations.telegram import dp
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
--- random_coffee_fe/intagrations/telegram.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Bot, Dispatcher
-from random_coffee_fe.config import TOKEN
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
